dac80e65b01dad2592800ec355ce6f321c2a5c9f
aAshutosh <being0023@gmail.com>
2024-08-30 10:22:30 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,112 @@
+import React, { useContext } from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+function Main() {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); 
+      onSent(); 
+  };
+}
+
+const handleCardClick = (text) => {
+   setInput(text);  // Set the card's text as input
+   onSent();  // Trigger the search
+ };
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello,Bro</span>
+              </p>
+              <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip.</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Tell me how can i increase productivity?</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Suggest me some good recepies without oil.</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Write the code for the two number swap without using third in c++.</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here."
+              onKeyDown={handleKeyDown}
+            />
+
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input ? (
+                <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check its responses.Your privacy and Gemini App
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
